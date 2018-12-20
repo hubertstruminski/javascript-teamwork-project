39fb604e6664d8ea67542030051fe1c504b6a031
franiczio <michal.fr@nelak.pl>
2018-12-20 10:04:55 +0100

    turning and moving

--- JS/Learn.js
+++ JS/Learn.js
@@ -5,10 +5,10 @@ var running = false;
 
 
     var car = {
-        x:0,
-        y:0,
-        radius:1,
-        v:0.1,
+        x:100,
+        y:100,
+        radius:0,
+        v:0,
         draw: function(){
          clear();
          view = new Image();
@@ -16,6 +16,10 @@ var running = false;
          ctx.drawImage(view,car.x+car.v*Math.pow((Math.cos(car.radius/360)),2),car.y+car.v*Math.pow((Math.sin(car.radius/360)),2),50,50);
          car.x=car.x+car.v*(Math.cos(car.radius));
          car.y=car.y+car.v*(Math.sin(car.radius));
+         if(car.v>0)
+            car.v-=0.001;
+         else
+            car.v+=0.001;
         }
     };
 
@@ -24,67 +28,75 @@ var running = false;
     }
 
     function speedUp(){
-            clear();
-            car.draw();
-            car.v+=0.01;
-
-
-        }
+                if(car.v<0)
+                    car.v+=0.035;
+                else;
+                car.v+=0.02;
+     }
     function speedDown(){
-            clear();
-            car.draw();
+
+
             if(car.v>0)
-                car.v-=0.025;
+                car.v-=0.035;
             else
-                car.v-=0.01;
+                car.v-=0.02;
 
 
 
     }
-
+// ctx.translate(car.x,car.y);
     function turnLeft(){
+
         car.radius-=0.1;
         if(car.radius<-360)
             car.radius=360;
-        console.log(car.radius);
-        console.log(car.v*(Math.cos(car.radius/360)));
+        //console.log(car.radius);
+        //console.log(car.v*(Math.cos(car.radius/360)));
+
+//        ctx.rotate(Math.PI/180);
+
+
     }
 
-
     function turnRight(){
+
         car.radius+=0.1;
         if(car.radius>359)
             car.radius=-359;
         console.log(car.radius);
         console.log(car.v*(Math.sin(car.radius/360)));
+
     }
 
-window.addEventListener('keydown', keyIsDown,true);
 
-function keyIsDown(e){
-if(e.keyCode==38){
-            speedUp();
-            if(e.keyCode==37){
-                turnLeft();
-            }
-        }
-        if(e.keyCode==40){
-            speedDown();
+window.addEventListener('keydown', function(e){
 
-        }
-        if(e.keyCode==37){
-            turnLeft();
+         if(e.keyCode==38){
+             speedUp();
+//            if(e.keyCode==37){
+//                turnLeft(true);
+//            }
+     }
+     if(e.keyCode==40){
+         speedDown();
 
-        }
+     }
+     if(e.keyCode==37){
+         turnLeft();
 
-        if(e.keyCode==39){
-            turnRight();
-        }
-}
+     }
+
+     if(e.keyCode==39){
+         turnRight();
+     }
+     },false);
 
 
 
-setInterval(car.draw,1);
+
+
+
+setInterval(car.draw,10);
 
 
 car.draw();
