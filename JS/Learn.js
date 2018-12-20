a4f9888f9da34f5db304e68aeec23dce61bddee1
franiczio <michal.fr@nelak.pl>
2018-12-20 12:08:50 +0100

    car colision with map borders

--- JS/Learn.js
+++ JS/Learn.js
@@ -9,11 +9,25 @@ var running = false;
         y:100,
         radius:0,
         v:0,
+        carHight:50,
+        carWidth:50,
         draw: function(){
          clear();
+        colisionMap();
          view = new Image();
-         view.src = "../JPG/car.png";
-         ctx.drawImage(view,car.x+car.v*Math.pow((Math.cos(car.radius/360)),2),car.y+car.v*Math.pow((Math.sin(car.radius/360)),2),50,50);
+        if(car.radius<Math.PI/4 && car.radius>=0 || car.radius>(7*Math.PI)/4 ) {
+            view.src = "../JPG/car.png";
+         }
+         else if(car.radius<(3*Math.PI)/4 && car.radius>(Math.PI)/4){
+            view.src = "../JPG/carDown.png";
+         }
+         else if(car.radius<(5*Math.PI)/4 && car.radius>(3*Math.PI)/4){
+            view.src = "../JPG/carLeft.png";
+         }
+         else if(car.radius<(7*Math.PI)/4 && car.radius>(5*Math.PI)/4){
+            view.src = "../JPG/carUp.png";
+         }
+         ctx.drawImage(view,car.x+car.v*Math.pow((Math.cos(car.radius/360)),2),car.y+car.v*Math.pow((Math.sin(car.radius/360)),2),car.carHight,car.carWidth);
          car.x=car.x+car.v*(Math.cos(car.radius));
          car.y=car.y+car.v*(Math.sin(car.radius));
          if(car.v>0)
@@ -23,6 +37,29 @@ var running = false;
         }
     };
 
+    function colisionMap(){
+        if (car.x<0){
+            car.v=0;
+            car.x=1;
+        }
+        if (car.y<0){
+            car.v=0;
+            car.y=1;
+        }
+        if(car.x>canvas.width-car.carWidth){
+            car.v=0;
+            car.x=canvas.width-car.carWidth-1;
+        }
+        if(car.y>canvas.height-car.carHight){
+            car.v=0;
+            car.y=canvas.height-car.carHight-1;
+        }
+        console.log(car.x);
+        console.log(canvas.width);
+        console.log();
+        console.log(car.y);
+    }
+
     function clear(){
         ctx.clearRect(0,0, canvas.width, canvas.height);
     }
@@ -30,8 +67,8 @@ var running = false;
     function speedUp(){
                 if(car.v<0)
                     car.v+=0.035;
-                else;
-                car.v+=0.02;
+                else
+                    car.v+=0.02;
      }
     function speedDown(){
 
@@ -48,12 +85,9 @@ var running = false;
     function turnLeft(){
 
         car.radius-=0.1;
-        if(car.radius<-360)
-            car.radius=360;
-        //console.log(car.radius);
-        //console.log(car.v*(Math.cos(car.radius/360)));
-
-//        ctx.rotate(Math.PI/180);
+        if(car.radius<0)
+            car.radius=2*Math.PI;
+        console.log(car.radius);
 
 
     }
@@ -61,21 +95,18 @@ var running = false;
     function turnRight(){
 
         car.radius+=0.1;
-        if(car.radius>359)
-            car.radius=-359;
+        if(car.radius>2*Math.PI)
+            car.radius=0;
         console.log(car.radius);
-        console.log(car.v*(Math.sin(car.radius/360)));
 
     }
 
 
 window.addEventListener('keydown', function(e){
 
-         if(e.keyCode==38){
-             speedUp();
-//            if(e.keyCode==37){
-//                turnLeft(true);
-//            }
+     if(e.keyCode==38){
+         speedUp();
+
      }
      if(e.keyCode==40){
          speedDown();
@@ -83,6 +114,7 @@ window.addEventListener('keydown', function(e){
      }
      if(e.keyCode==37){
          turnLeft();
+
 
      }
 
